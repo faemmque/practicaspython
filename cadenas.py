ad52884171adae28307065fe9e4c6a8b5a387e63
Fausto Emmanuel <fausto.quevedo@gmail.com>
2025-05-08 08:02:50 -0600

    Se inicia practica con cadenas

---
+++ cadenas.py
@@ -0,0 +1,60 @@
+# texto_variado = "Palabra 123 +-* #$%"
+# print(texto_variado)
+
+# print("""
+# Funcionamiento de \
+# programa: (Opciones)
+# 1 - Para acceder a opciones
+#     2 - Para salir
+#       """)
+
+#Subsscripting e indexado
+
+# texto =  "Python"
+# print(texto[0])
+# print(texto[5])
+# print(texto[-1])
+# print(texto[-6])
+
+# print(texto[6]) # Error de indice
+# print(texto[-7]) # Error de indice
+
+# letra =  texto[0]
+# print(letra)
+# # texto[0] = 'p' # Error: no se puede modificar una cadena
+# letra = 'p'
+# print(letra)
+
+# texto_compuesto =  letra + texto[1]
+# print(texto_compuesto)
+
+###########################################################################################
+
+# Slicing o substring
+
+# texto = "Python"
+
+# print(texto[0:3])
+# print(texto[0:-3])
+# print(texto[0:-2])
+# print(texto[2:])
+# print(texto[:3])
+# print(texto[-3::-1])
+# print(texto[::-1])
+
+# print(texto[1:50])
+# print(texto[2:2])
+
+# texto = "Hola mundo! Buenastardes"
+
+# print(texto.lower())
+# print(texto.upper())
+# print(texto.capitalize())
+# print(texto.title())
+# print(texto.swapcase())
+
+print("{} + {} = {}".format(2,3,2+3))
+print("{} + {} = {}".format("hola","mundo","hola mundo"))
+print("{:.3f} + {:.4f} = {}".format(2,3,2+3))
+print("{1} + {0} = {2}".format(2,3,2+3))
+
