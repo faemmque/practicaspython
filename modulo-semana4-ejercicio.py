dad63a3b091fe6ae53290d00004325d5dce5b6c7
Fausto Emmanuel <fausto.quevedo@gmail.com>
2025-05-08 20:53:40 -0600

    ejercicio de semana 4

---
+++ modulo-semana4-ejercicio.py
@@ -0,0 +1,13 @@
+
+
+nombre = input("Introduce tu nombre: ")
+apellidos = input("Introduce tus apellidos: ")
+edad = int(input("Introduce tu edad: "))
+correo = input("Introduce tu correo electronico: ")
+telefono = input("Introduce tu teléfono: ")
+
+print("Nombre: " + nombre)
+print("Apellidos: " + apellidos)
+print("Tengo: " + str(edad) + " años")
+print("Correo: " + correo)
+print("Teléfono: " + telefono)
